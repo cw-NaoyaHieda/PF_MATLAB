69554e94f01ea2ecc5f3cd6b788c1ba9d857882d
naoya12185 <hero12185@gmail.com>
2018-07-24 10:47:18 +0900

    ite3 add

--- main.m
+++ main.m
@@ -21,7 +21,7 @@ dT = 100;
 X = ones(dT,1,'gpuArray');
 DR = ones(dT,1,'gpuArray');
 
-opt_params = zeros(900,6);
+opt_params = zeros(9000,7);
 
 for ite = 1:30
   X(1) = sqrt(beta)*X_0 + sqrt(1 - beta) * random('Normal',0,1);
@@ -30,14 +30,15 @@ for ite = 1:30
     DR(i) = r_DR(X(i-1),q_qnorm, rho, beta);
   end
   %DR(1) = DR(2)*(random('Normal',0,1)*0.05+1);
-  beta_est = (1 - 0.5) * rand(1) + 0.5;
+  for ite3 = 1:10
+  beta_est = (0.95 - 0.7) * rand(1) + 0.7;
   q_qnorm_est = (-3 + 2) * rand(1) + -1.5;
-  rho_est = 0.20 * rand(1);
+  rho_est = (0.20 - 0.01) * rand(1) + 0.01;
   %X_0_est = (-3 + 2) * rand(1) + -2;
   %first_pm = [beta_est,q_qnorm_est,rho_est,X_0_est];
   %opt_params((ite-1) * 30 + 1,:) =  [ite,1,first_pm,1000];
   first_pm = [beta_est,q_qnorm_est,rho_est];
-  opt_params((ite-1) * 30 + 1,:) =  [ite,1,first_pm,1000];
+  opt_params((ite-1) * 300 + (ite3 - 1)*30 + 1,:) =  [ite,ite3,1,first_pm,1000];
   for ite2 = 2:30
   [filter_X, filter_weight, filter_X_mean] = particle_filter(N, dT, DR, beta_est, q_qnorm_est, rho_est, X_0);
   [sm_weight, sm_X_mean] = particle_smoother(N, dT, beta_est, filter_X, filter_weight);
@@ -45,17 +46,19 @@ for ite = 1:30
   PMCEM = @(params)Q_calc_nf(params, X_0, dT, pw_weight, filter_X, sm_weight, DR);
   first_pm = [beta_est,q_qnorm_est,rho_est];
   [params,fval] = fminunc(PMCEM,first_pm)
-  opt_params((ite-1) * 30 + ite2,:) =  [ite,ite2,params,fval];
+  opt_params((ite-1) * 300 + (ite3 - 1)*30 + ite2,:) =  [ite,ite3,ite2,params,fval];
   beta_est = params(1);
   q_qnorm_est = params(2);
   rho_est = params(3);
   ite2
   end
+  ite3
+  end
   ite
 end
 
 
-csvwrite("data/opt_params_100.csv",opt_params);
+csvwrite("data/opt_params_100_10.csv",opt_params);
 %csvwrite("data/matlab_X.csv",X);
 %csvwrite("data/matlab_DR.csv",DR);
 %data = csvread("data/X.csv");

264692d6b29a15d340dfd512fb2f0b4c9e8b4cc1
naoya12185 <hero12185@gmail.com>
2018-07-26 15:30:48 +0900

    function change

--- functions/particle_smoother.m
+++ functions/particle_smoother.m
@@ -1,22 +1,40 @@
 %平滑化
-function [sm_weight, sm_X_mean] = particle_smoother(N, dT, beta_est, filter_X, filter_weight)
+function [sm_X, sm_weight, sm_X_mean] = particle_smoother(N, dT, beta_est, filter_X, filter_weight)
+  sm_X = zeros(dT - 1, N,'gpuArray');
   sm_weight = zeros(dT - 1, N,'gpuArray');
   sm_weight_tmp = zeros(1,N,'gpuArray');
   sm_X_mean = ones(dT - 1, 1,'gpuArray');
   %T時点のweightは変わらないのでそのまま代入
+  sm_X(dT - 1,:) = filter_X(dT - 1,:);
   sm_weight(dT - 1,:) = filter_weight(dT - 1,:);
 	sm_X_mean(dT - 1) = sm_weight(dT - 1,:)*filter_X(dT - 1,:)';
 	for dt = (dT - 2):-1:1
 	    sum_weight = 0;
 	    %分子計算 横にnormpdfの分子が変化、縦に分母が変化
-	    bunsi = sm_weight(dt + 1,:) * normpdf([filter_X(dt + 1,:)], [sqrt(beta_est) *  filter_X(dt, :)]', sqrt(1 - beta_est))';
+	    bunsi = sm_weight(dt + 1,:) * normpdf([sm_X(dt + 1,:)], [sqrt(beta_est) *  filter_X(dt, :)]', sqrt(1 - beta_est))';
 	    %分母計算 横にnormpdfの分子が変化、縦に分母が変化    
-	    bunbo = filter_weight(dt,:) * normpdf([filter_X(dt + 1,:)], [sqrt(beta_est) * filter_X(dt,:)]', sqrt(1 - beta_est));
+	    bunbo = filter_weight(dt,:) * normpdf([sm_X(dt + 1,:)], [sqrt(beta_est) * filter_X(dt,:)]', sqrt(1 - beta_est));
 
 			sm_weight_tmp = filter_weight(dt, :) .* bunsi ./ bunbo;
 	    %dt
-	    sm_weight(dt, :) = sm_weight_tmp / sum(sm_weight_tmp);
-	    sm_X_mean(dt) = sm_weight(dt,:)*filter_X(dt,:)';
+	    %sm_weight(dt, :) = sm_weight_tmp / sum(sm_weight_tmp);
+	    cs_weight = cumsum(sm_weight_tmp);
+      weight = sm_weight_tmp / cs_weight(N);
+      cs_weight = cs_weight / cs_weight(N);
+      resample_check_weight = weight.^2;
+      resample_check_weight = sum(resample_check_weight);
+      %リサンプリング (resample)とりあえず並列にしない
+      if 0;
+      dt;
+      weight = repmat(1 / N, N, 1);
+      resumple_num =  resample(cs_weight,rand(1,N) ,N);
+      sm_X(dt,:) = filter_X(dt,resumple_num);
+      else
+      sm_X(dt,:) = filter_X(dt,:);
+      end
+      sm_weight(dt, :) = weight;
+	    sm_X_mean(dt) = sm_weight(dt,:)*sm_X(dt,:)';
 	end
+	
 
 end
--- functions/r_DR.m
+++ functions/r_DR.m
@@ -1,7 +1,7 @@
 %関数定義
 %DRの乱数発生
 function y = r_DR(X_t, q_qnorm, rho, beta)
-  y = (q_qnorm - sqrt(rho)*sqrt(beta)*X_t) / sqrt(1 - rho) - sqrt(rho)*sqrt(1 - beta) / sqrt(1 - rho) * random('Normal',0,1);
+  y = (q_qnorm - sqrt(rho*beta)*X_t) / sqrt(1 - rho) - sqrt(rho*(1 - beta) / (1 - rho)) * random('Normal',0,1);
 end
 
 

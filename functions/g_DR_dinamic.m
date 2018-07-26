d0accb5c284423fb1d57b478de9581c80fd46a3b
naoya12185 <hero12185@gmail.com>
2018-07-26 15:46:56 +0900

    sigma
    標準偏差を二乗していたのを分散に変更

--- functions/g_DR_dinamic.m
+++ functions/g_DR_dinamic.m
@@ -2,5 +2,5 @@
 function y = g_DR_dinamic(tilde_DR, X_t_1, q_qnorm, beta, rho)
 y = 1 / (sqrt(rho*(1 - beta) / (1 - rho)) * sqrt(2 * pi)) *...
            exp(-(tilde_DR - (q_qnorm - sqrt(rho*beta)*X_t_1) / sqrt(1 - rho)).^2 /...
-                 (sqrt(rho*(1 - beta) / (1 - rho))^2*2));
+                 (rho*(1 - beta) / (1 - rho)*2));
 end
